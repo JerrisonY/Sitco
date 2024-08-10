d47ee49d2e0b597b805222b30ad9f5bed18bac4c
Jerrison Yip <jerryyip24@gmail.com>
2024-08-10 09:37:06 -0400

    initial files

---
+++ src/components/RotateDevice.jsx
@@ -0,0 +1,14 @@
+import './RotateDevice.css'
+
+function RotateDevice() {
+  return (
+    <div id='landscape-msg'>
+        <div className="phone"></div>
+        <div className="message">
+          Please rotate your device!
+        </div>
+      </div>
+  )
+}
+
+export default RotateDevice
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.scss'
+import { NavLink } from 'react-router-dom'
+
+import RotateDevice from '../../components/RotateDevice.jsx'
+
+function Home() {
+  return (
+    <>
+        <RotateDevice />
+    </>
+  )
+}
+
+export default Home
