f49374c99f039a37d31e0f83de878f629d27eebb
Jerrison Yip <jerryyip24@gmail.com>
2024-08-22 16:52:08 -0400

    added hero text styles

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import './HeroSection.scss'
+
+import heroImg from '../assets/img/landingimg.jpeg'
+
+function HeroSection() {
+  return (
+    <div className='hero-c'>
+        <div className='hero-text-c'>
+            <h1>SITCO Precision Machining</h1>
+            <p className='hero-sub'>Expertise in Precision EDM Machining and Solutions Since 1978</p>
+            <p className='hero-btn'>Get in touch with us now</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default HeroSection
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import './Home.scss'
 import { NavLink } from 'react-router-dom'
 
+import HeroSection from '../../components/HeroSection'
 
 function Home() {
   return (
     <>
-
+      <HeroSection />
 
     </>
   )
