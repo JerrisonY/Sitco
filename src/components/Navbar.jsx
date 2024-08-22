e9c432861a8eddf635141291a11ab9d3ecdfb774
Jerrison Yip <jerryyip24@gmail.com>
2024-08-22 14:10:50 -0400

    added navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import './Navbar.scss'
-
 import { NavLink } from 'react-router-dom'
 
+import SitcoLogo from '/Sitco.svg'
 
 function Navbar() {
   return (
     <nav className='nav-container'>
         <NavLink to={'/'}>
-            Logo
+            <img src={SitcoLogo} alt="" style={{height: '40px'}}/>
         </NavLink>
         <div className='nav-links'>
-            <p>link</p>
-            <p>link</p>
-            <p>link</p>
+            <NavLink>About Us</NavLink>
+            <NavLink>Services</NavLink>
+            <NavLink>Quality</NavLink>
+            <NavLink>Contact Us</NavLink>
         </div>
     </nav>
   )
