788b2d9727ac879ea8736052aa5c346d2dbe290f
Jerrison Yip <jerryyip24@gmail.com>
2024-08-22 18:46:51 -0400

    hero section styling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,12 @@
 import './HeroSection.scss'
 
-import heroImg from '../assets/img/landingimg.jpeg'
-
 function HeroSection() {
   return (
     <div className='hero-c'>
         <div className='hero-text-c'>
             <h1>SITCO Precision Machining</h1>
             <p className='hero-sub'>Expertise in Precision EDM Machining and Solutions Since 1978</p>
-            <p className='hero-btn'>Get in touch with us now</p>
+            <a className='hero-btn'>Get in touch with us now</a>
         </div>
 
     </div>
