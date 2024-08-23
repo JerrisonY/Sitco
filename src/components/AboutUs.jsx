acbde5eb72ff3efe2a8e5a46f3577b6dee87800e
Jerrison Yip <jerryyip24@gmail.com>
2024-08-22 20:43:54 -0400

    added img and more copy

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,15 +1,41 @@
 import './AboutUs.scss'
 
-import { ABOUT_HISTORY } from '../lib/constants'
-import { ABOUT_EXPERTISE } from '../lib/constants'
+import { 
+    ABOUT_HISTORY, 
+    ABOUT_EXPERTISE, 
+    ABOUT_EXPERTISE_TWO,
+    ABOUT_EDM_ONE,
+    ABOUT_EDM_TWO,
+    ABOUT_EDM_THREE, 
+    } from '../lib/constants'
+
+import imgOne from '../assets/img/about-img-1.png'
+import imgTwo from '../assets/img/about-img-2.jpg'
 
 function AboutUs() {
   return (
     <div className='about-c'>
         <h1>About Us</h1>
         <hr />
+        <p className='section-subhead'>HISTORY</p>
         <p>{ABOUT_HISTORY}</p>
+
+        <p className="section-subhead">EXPERTISE</p>
         <p>{ABOUT_EXPERTISE}</p>
+        <br />
+        <p>{ABOUT_EXPERTISE_TWO}</p>
+
+        <div className='about-img-c'>
+            <img src={imgOne} alt="" />
+            <img src={imgTwo} alt="" />
+        </div>
+
+        <p className="section-subhead">EDM</p>
+        <p>{ABOUT_EDM_ONE}</p>
+        <br />
+        <p>{ABOUT_EDM_TWO}</p>
+        <br />
+        <p>{ABOUT_EDM_THREE}</p>
     </div>
   )
 }
