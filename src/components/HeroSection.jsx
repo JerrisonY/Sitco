5586798661f73f3273f8697c6fd1e38a26884bc7
Jerrison Yip <jerryyip24@gmail.com>
2024-08-26 16:29:06 -0400

    changed img to be imported from jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,14 @@
 import './HeroSection.scss'
 
+import landingImg from '../assets/img/landingimg.jpeg';
+
 function HeroSection() {
   return (
-    <div className='hero-c' id='hero'>
+    <div className='hero-c' id='hero'
+      style={{
+        backgroundImage: `url(${landingImg})`
+      }}
+    >
         <div className='hero-text-c'>
             <h1>SITCO Precision Machining</h1>
             <p className='hero-sub'>Expertise in Precision EDM Machining and Solutions Since 1978</p>
